7aeeef2a0395d64114c92945977af777081724eb
yildirimuguradesso <ugur.yildirim@adesso.com.tr>
2023-09-06 09:15:21 +0300

    Angular Basics - Creating header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ServerComponent } from './server/server.component';
+import { HeaderModule } from './components/header/header.module'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HeaderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+    declarations: [
+      NavComponent,
+    ],
+    imports: [
+    ],
+    providers: [],
+  })
+  
+  export class HeaderModule { }
